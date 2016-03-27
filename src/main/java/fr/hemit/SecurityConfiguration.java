6bf392201072e436d9c053414b6d152109c402d5
fhemery <fhemery@hemit.fr>
2016-03-28 01:02:17 +0200

    Update configuration in order to authorize properly user.
    The csrf is currently disabled. A closer look will be needed to enable it later on.

--- src/main/java/fr/hemit/SecurityConfiguration.java
+++ src/main/java/fr/hemit/SecurityConfiguration.java
@@ -1,16 +1,32 @@
 package fr.hemit;
 
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
 @Configuration
 @EnableWebSecurity
@@ -41,12 +57,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/api/user/").permitAll()					// Allow user creation
-				.antMatchers("/api/", "/api/**").access("hasRole('USER')")
-			.and().httpBasic()
-			//.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and().csrf().disable();
+		http.httpBasic()
+			.and()
+				.authorizeRequests()
+					.antMatchers("/api/user/", "/api/").permitAll()
+					.antMatchers("/api/novel**").access("hasRole('USER')")
+			.and().csrf().disable()
+			.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
+			
 
+	}
+	
+
+	public class CsrfHeaderFilter extends OncePerRequestFilter {
+		@Override
+		protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+				FilterChain filterChain) throws ServletException, IOException {
+			response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+			
+			filterChain.doFilter(request, response);
+		}
 	}
 }
